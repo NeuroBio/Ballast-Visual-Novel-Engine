af914e0d1c5ee0842f24a36573f611464ddbf830
kArA <kArARedwing@gmail.com>
2024-07-20 06:36:48 -0400

    finsih multi response play

--- src/Beat/MultiResponseBeat.ts
+++ src/Beat/MultiResponseBeat.ts
@@ -1,4 +1,4 @@
-import { Beat, FinalBeatDisplay, PlayParams, StandardBeatDisplay } from './Beat';
+import { Beat, PlayParams, StandardBeatDisplay } from './Beat';
 
 
 interface DefaultBehavior {
@@ -7,8 +7,14 @@ interface DefaultBehavior {
 	nextBeat: string;
 }
 
+interface ResponseBehavior {
+	text: string;
+	character?: string;
+	nextBeat?: string;
+}
+
 interface Response {
-	beat: StandardBeatDisplay | FinalBeatDisplay;
+	beat: ResponseBehavior;
 	conditions: Array<(params: PlayParams) => boolean>;
 }
 
@@ -29,6 +35,7 @@ export class MultiResponseBeat extends Beat {
 		super(params);
 		this.#responses = responses;
 		this.#defaultBehavior = defaultBehavior;
+		this.#playableOptions = [];
 
 		if (responses.length < 2) {
 			throw new Error('Multi Response Beats require at least 2 responses.');
@@ -36,13 +43,46 @@ export class MultiResponseBeat extends Beat {
 	}
 
 	play (params: PlayParams): StandardBeatDisplay {
-		const { characters, inventory } = params;
-		// if !lastPlayed, assemble playableOptions
-		// 		no playable options, play default
-		// 		some, set playNext to 0
-		// assemble next beat (nextBeat = self (not last beat) OR defaultBehavior.nextBeat (last beat))
-		// return response
+		const { characters } = params;
 
-		return { text: '', nextBeat: '' };
+		if (!this.#lastPlayed) {
+			this.#lastPlayed = 0;
+
+			this.#responses.forEach((response) => {
+				if (response.conditions.every((condition) => condition(params))) {
+					this.#playableOptions.push(response);
+				}
+			});
+		}
+
+		const nextResponse = this.#playableOptions[this.#lastPlayed];
+		this.#lastPlayed += 1;
+		if (nextResponse) {
+			const character = this.getCharacter({
+				character: nextResponse.beat.character,
+				characters,
+			});
+			return {
+				text: `${character}: ${nextResponse.beat.text}`,
+				nextBeat: nextResponse.beat.nextBeat || this.#getFallback(),
+			};
+		}
+
+		const character = this.getCharacter({
+			character: this.#defaultBehavior.character,
+			characters,
+		});
+		return {
+			text: `${character}: ${this.#defaultBehavior.text}`,
+			nextBeat: this.#defaultBehavior.nextBeat,
+		};
+	}
+
+	#getFallback (): string {
+		if (this.#lastPlayed === this.#playableOptions.length) {
+			return this.#defaultBehavior.nextBeat;
+		}
+
+		return this.key;
 	}
 }
--- test/unit/spec/Beat/MultiReponseBeat/play.spec.ts
+++ test/unit/spec/Beat/MultiReponseBeat/play.spec.ts
@@ -1,3 +1,4 @@
+import { NARRATOR } from '../../../../../src/Beat/Beat';
 import { MultiResponseBeat } from '../../../../../src/Beat/MultiResponseBeat';
 import { Character } from '../../../../../src/Character/Character';
 import { CharacterData } from '../../../FakeData/TestData';
@@ -8,11 +9,11 @@ describe(`MultiResponseBeat.play`, () => {
 		return keyed;
 	}, {});
 	describe(`
-		playing a beat with two non-conditional responses
-		and no set next beat
+		playing a beat with two non-conditional responses without characters
+		and no set next beats
 		for the first time
 	`, () => {
-		it(`returns the multi response beat's key`, () => {
+		it(`returns the multi response beat's key with the first beat's text`, () => {
 			const parentBeatKey = 'parent';
 			const defaultText = 'default';
 			const defaultNextBeat = 'defaultBeat';
@@ -25,10 +26,156 @@ describe(`MultiResponseBeat.play`, () => {
 			});
 
 			expect(beat.play({ characters: keyedCharacters, inventory: {} })).toEqual({
-				text: '',
+				text: `${NARRATOR}: ${response1.beat.text}`,
+				nextBeat: parentBeatKey,
+			});
+		});
+	});
+	describe(`
+		playing a beat with two non-conditional responses without characters
+		and no set next beats
+		for the second time
+	`, () => {
+		it(`returns the multi response beat's default behavior key with the second beat's text`, () => {
+			const parentBeatKey = 'parent';
+			const defaultText = 'default';
+			const defaultNextBeat = 'defaultBeat';
+			const response1 = { beat: { text: 'first' }, conditions: [] };
+			const response2 = { beat: { text: 'second' }, conditions: [] };
+			const beat = new MultiResponseBeat({
+				key: parentBeatKey,
+				responses: [response1, response2],
+				defaultBehavior: { text: defaultText, nextBeat: defaultNextBeat },
+			});
+			beat.play({ characters: keyedCharacters, inventory: {} });
+
+			expect(beat.play({ characters: keyedCharacters, inventory: {} })).toEqual({
+				text: `${NARRATOR}: ${response2.beat.text}`,
 				nextBeat: defaultNextBeat,
 			});
 		});
 	});
-	// fill out missing scenarios
+	describe(`
+		playing a beat with two non-conditional responses without characters
+		and set next beats
+		for the first time
+	`, () => {
+		it(`returns data from the first response`, () => {
+			const parentBeatKey = 'parent';
+			const defaultText = 'default';
+			const defaultNextBeat = 'defaultBeat';
+			const response1 = { beat: { text: 'first', nextBeat: 'firstNext' }, conditions: [] };
+			const response2 = { beat: { text: 'second', nextBeat: 'nextNext' }, conditions: [] };
+			const beat = new MultiResponseBeat({
+				key: parentBeatKey,
+				responses: [response1, response2],
+				defaultBehavior: { text: defaultText, nextBeat: defaultNextBeat },
+			});
+
+			expect(beat.play({ characters: keyedCharacters, inventory: {} })).toEqual({
+				text: `${NARRATOR}: ${response1.beat.text}`,
+				nextBeat: response1.beat.nextBeat,
+			});
+		});
+	});
+	describe(`
+		playing a beat with two non-conditional responses without characters
+		and set next beats
+		for the second time
+	`, () => {
+		it(`returns data from the second response`, () => {
+			const parentBeatKey = 'parent';
+			const defaultText = 'default';
+			const defaultNextBeat = 'defaultBeat';
+			const response1 = { beat: { text: 'first', nextBeat: 'firstNext' }, conditions: [] };
+			const response2 = { beat: { text: 'second', nextBeat: 'nextNext' }, conditions: [] };
+			const beat = new MultiResponseBeat({
+				key: parentBeatKey,
+				responses: [response1, response2],
+				defaultBehavior: { text: defaultText, nextBeat: defaultNextBeat },
+			});
+			beat.play({ characters: keyedCharacters, inventory: {} });
+
+			expect(beat.play({ characters: keyedCharacters, inventory: {} })).toEqual({
+				text: `${NARRATOR}: ${response2.beat.text}`,
+				nextBeat: response2.beat.nextBeat,
+			});
+		});
+	});
+	describe(`
+		playing a beat with three conditional responses without characters
+		and only the second condition is met
+		for the first time
+	`, () => {
+		it(`returns the multi response beat's key with the second beat's text`, () => {
+			const parentBeatKey = 'parent';
+			const defaultText = 'default';
+			const defaultNextBeat = 'defaultBeat';
+			const response1 = { beat: { text: 'first' }, conditions: [() => false] };
+			const response2 = { beat: { text: 'second' }, conditions: [() => true] };
+			const response3 = { beat: { text: 'second' }, conditions: [() => false] };
+			const beat = new MultiResponseBeat({
+				key: parentBeatKey,
+				responses: [response1, response2, response3],
+				defaultBehavior: { text: defaultText, nextBeat: defaultNextBeat },
+			});
+
+			expect(beat.play({ characters: keyedCharacters, inventory: {} })).toEqual({
+				text: `${NARRATOR}: ${response2.beat.text}`,
+				nextBeat: defaultNextBeat,
+			});
+		});
+	});
+	describe(`
+		playing a beat with three conditional responses without characters
+		and no set next beats
+		and only the second condition is met
+		for the second time
+	`, () => {
+		it(`returns the multi response beat's default behavior key with the third beat's text`, () => {
+			const parentBeatKey = 'parent';
+			const defaultText = 'default';
+			const defaultNextBeat = 'defaultBeat';
+			const response1 = { beat: { text: 'first' }, conditions: [() => false] };
+			const response2 = { beat: { text: 'second' }, conditions: [() => true] };
+			const response3 = { beat: { text: 'second' }, conditions: [() => false] };
+			const beat = new MultiResponseBeat({
+				key: parentBeatKey,
+				responses: [response1, response2, response3],
+				defaultBehavior: { text: defaultText, nextBeat: defaultNextBeat },
+			});
+			beat.play({ characters: keyedCharacters, inventory: {} });
+
+			expect(beat.play({ characters: keyedCharacters, inventory: {} })).toEqual({
+				text: `${NARRATOR}: ${defaultText}`,
+				nextBeat: defaultNextBeat,
+			});
+		});
+	});
+	describe(`
+		playing a beat with three conditional responses without characters
+		and no set next beats
+		no condition is met
+		for the first time
+	`, () => {
+		it(`returns the multi response beat's default behavior`, () => {
+			const parentBeatKey = 'parent';
+			const defaultText = 'default';
+			const defaultNextBeat = 'defaultBeat';
+			const response1 = { beat: { text: 'first' }, conditions: [() => false] };
+			const response2 = { beat: { text: 'second' }, conditions: [() => false] };
+			const response3 = { beat: { text: 'second' }, conditions: [() => false] };
+			const beat = new MultiResponseBeat({
+				key: parentBeatKey,
+				responses: [response1, response2, response3],
+				defaultBehavior: { text: defaultText, nextBeat: defaultNextBeat },
+			});
+			beat.play({ characters: keyedCharacters, inventory: {} });
+
+			expect(beat.play({ characters: keyedCharacters, inventory: {} })).toEqual({
+				text: `${NARRATOR}: ${defaultText}`,
+				nextBeat: defaultNextBeat,
+			});
+		});
+	});
 });
