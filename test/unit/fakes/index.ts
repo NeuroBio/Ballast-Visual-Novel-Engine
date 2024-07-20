1bd17ab4fed20cc9bdc777947a153f09726b70d5
kArA <kArARedwing@gmail.com>
2024-07-20 07:12:25 -0400

    remove unused mock

--- test/unit/fakes/index.ts
+++ test/unit/fakes/index.ts
@@ -2,7 +2,6 @@ import { FinalBeat, SimpleBeat } from './Beat';
 import { BeatFactory } from './BeatFactory';
 import { Chapter } from './Chapter';
 import { ChapterFinder } from './ChapterFinder';
-import { Character } from './Character';
 import { CharacterTemplateFinder } from './CharacterTemplateFinder';
 import { SavedData } from './SavedData';
 import { SavedDataRepo } from './SavedDataRepo';
@@ -11,7 +10,6 @@ import { SceneFinder } from './SceneFinder';
 
 export const Fakes = {
 	CharacterTemplateFinder,
-	Character,
 	Chapter,
 	ChapterFinder,
 	SceneFinder,
