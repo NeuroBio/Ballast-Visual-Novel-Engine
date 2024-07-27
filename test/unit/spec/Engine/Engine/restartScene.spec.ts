9ed01b72115578f92bf0befca2dd4101d01aadf0
kArA <kArARedwing@gmail.com>
2024-07-27 09:19:49 -0400

    finish restart testing

---
+++ test/unit/spec/Engine/Engine/restartScene.spec.ts
@@ -0,0 +1,78 @@
+import { Fakes } from '../../../fakes/index';
+import { NARRATOR } from '../../../../../src/Beat/Beat';
+import { Engine } from '../../../../../src/Engine/Engine';
+import { CharacterData, SavedDataData } from '../../../FakeData/TestData';
+
+describe(`Engine.restart`, () => {
+	const firstBeatKey = 'firstBeat';
+	const firstBeatDto = {
+		key: firstBeatKey,
+		responses: [
+			{ text: 'response 1' },
+			{ text: 'response 2' },
+		],
+		defaultBehavior: { text: 'no play', nextBeat: 'no play' },
+	};
+	const sceneDto = {
+		beats: [firstBeatDto],
+		firstBeatKey,
+		name: 'Scene Name',
+		key: 'sceneKey',
+	};
+	const chapterDto = {
+		key: 'chapKey',
+		name: 'chap',
+		locked: false,
+		firstSceneKey: sceneDto.key,
+		sceneKeys: [sceneDto.key],
+	};
+
+	let savedDataRepoFake: any, characterTemplateFinderFake: any;
+	async function _createEngine (): Promise<Engine> {
+		savedDataRepoFake = new Fakes.SavedDataRepo();
+		savedDataRepoFake.findOrCreate.mockReturnValueOnce(new Fakes.SavedData());
+		characterTemplateFinderFake = new Fakes.CharacterTemplateFinder();
+		const engine = new Engine({
+			findChapterData: () => Promise.resolve([chapterDto]),
+			findSceneData: () => Promise.resolve([sceneDto]),
+			findCharacterData: () => Promise.resolve(CharacterData),
+			findSavedData: () => Promise.resolve(SavedDataData),
+			saveSavedData: () => Promise.resolve(),
+			characterTemplateFinder: characterTemplateFinderFake,
+			savedDataRepo: savedDataRepoFake,
+			// intentionally using the real scene and chapter finders for this
+		});
+		return engine;
+	}
+	it(`recreates the scene with fresh beat data to reset multi-response beats`, async () => {
+		const engine = await _createEngine();
+
+		const firstResponse = await engine.startChapter({ chapterKey: chapterDto.key });
+		expect(firstResponse).toEqual({
+			text: firstBeatDto.responses[0].text,
+			nextBeat: firstBeatKey,
+			speaker: NARRATOR,
+			sceneData: expect.any(Object),
+			saveData: expect.any(Object),
+		});
+
+		const secondResponse = engine.advanceScene({ beatKey: firstBeatKey });
+		expect(secondResponse).toEqual({
+			text: firstBeatDto.responses[1].text,
+			nextBeat: firstBeatDto.defaultBehavior.nextBeat,
+			speaker: NARRATOR,
+			sceneData: expect.any(Object),
+			saveData: expect.any(Object),
+		});
+
+
+		const replayResponse = await engine.restartScene();
+		expect(replayResponse).toEqual({
+			text: firstBeatDto.responses[0].text,
+			nextBeat: firstBeatKey,
+			speaker: NARRATOR,
+			sceneData: expect.any(Object),
+			saveData: expect.any(Object),
+		});
+	});
+});
